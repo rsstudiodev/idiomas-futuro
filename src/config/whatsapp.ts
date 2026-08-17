dd6badc2a8305b5312019559869b577c50c98fdd
Rs Studio <rsstudiodev.gma@gmail.com>
2026-08-17 12:12:06 -0600

    Cambiar mensaje de whatsapp

---
+++ src/config/whatsapp.ts
@@ -0,0 +1,4 @@
+export const telefonoWhatsapp = "522206315612";
+export const mensajeWhatsapp = "Vi su página demo y me interesa trabajar con ustedes! ¿Me dan más información?";
+
+export const waHref = `https://wa.me/${telefonoWhatsapp}?text=${encodeURIComponent(mensajeWhatsapp)}`;
